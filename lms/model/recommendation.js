1ed9a6e3f27432e367321fae11fcd72382d651a0
HER2019 <raf.sus@mail.ru>
2019-08-12 17:45:00 +0400

    Added some classes

--- lms/model/recommendation.js
+++ lms/model/recommendation.js
@@ -4,10 +4,5 @@ class Recommendation {
         this.bookId = bookId;
         this.message = message;
     }
-    get UserID() {
-        return this.userId;
-    }
-    get BookID() {
-        return this.bookId;
-    }
+
 }
---
+++ lms/requests/request.js
@@ -0,0 +1,19 @@
+class Request{
+
+}
+
+class RequestForBookIssue extends Request{
+
+}
+class RequestForBookRenewal extends Request{
+
+}
+class RequestForBookReturn extends Request{
+
+}
+class RequestForHistory extends Request{
+
+}
+class RequestForBookRecommend extends Request{
+
+}
