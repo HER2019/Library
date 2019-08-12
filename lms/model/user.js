8ac4d6f35a4840ef69d3703aa53101ede94405a9
RafaelSus <53851697+RafaelSus@users.noreply.github.com>
2019-08-12 20:10:09 +0400

    Blya

--- lms/model/user.js
+++ lms/model/user.js
@@ -1,4 +1,44 @@
 class User {
+  static var cnt = 0;
+  constructor(permission, name, lastName){
+    this.name = name;
+    this.lastName = lastName;
+    this.permission = permission;
+    this.cnt++;
+    this.userId = cnt;
+    this.books = [];
+  }
 
+  getId(){
+    return this.userId;
+  }
 
+  viewHistory(){
+    return this.books;
+  }
+
+  request(num){
+    switch(num){
+      case 1:{
+        let request = new RequestForBookIssue(this.userId);
+        break;
+      }
+      case 2:{
+        let request = new RequestForBookRenewal(this.userId);
+        break;
+      }
+      case 3:{
+        let request = new RequestForBookReturn(this.userId);
+        break;
+      }
+      case 4:{
+        let request = new RequestForHistory(this.userId);
+        break;
+      }
+      case 5:{
+        let request = new RequestForBookRecommend(this.userId);
+        break;
+      }
+    }
+  }
 }
--- lms/requests/request.js
+++ lms/requests/request.js
@@ -4,16 +4,53 @@ class Request{
 
 class RequestForBookIssue extends Request{
 
+  constructor(userId){this.userId = userId;}
+
+  request(){
+    let bookName = getBookName();
+    library.addRequest(1, userId, bookName);
+  }
 }
+
+
 class RequestForBookRenewal extends Request{
 
+  constructor(userId){this.userId = userId;}
+
+  request(){
+    let bookId = getBookId();
+    library.addRequest(2, userId, bookId);
+  }
 }
+
+
 class RequestForBookReturn extends Request{
 
-}
-class RequestForHistory extends Request{
+  constructor(userId){this.userId = userId;}
 
+  request(){
+    let bookId = getBookId();
+    library.addRequest(3, userId, bookId);
+  }
 }
+
+
+class RequestForHistory extends Request{
+  constructor(userId){this.userId = userId;}
+
+  request(){
+    library.addRequest(4, userId);
+  }
+}
+
+
 class RequestForBookRecommend extends Request{
 
+  constructor(userId){this.userId = userId;}
+
+  request(){
+    let bookId = getBookId();
+    let recommend = getRecommend();
+    library.addRequest(5, userId, bookId, recommend);
+  }
 }
